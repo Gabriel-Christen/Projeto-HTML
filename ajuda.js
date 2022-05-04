1fdfb75770b7fd77fb4b314d4d5e73ec430f3fdd
Gabriel-Christen <bielchristen@gmail.com>
2022-05-03 21:54:39 -0300

    primeiro commit

---
+++ ajuda.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    var botao = $('ul.ajuda1');
+    var dropDown = $('.ajuda1des');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+   //Subtituir as 2 classes
+   $(document).ready(function() {
+    var botao = $('ul.ajuda2');
+    var dropDown = $('.ajuda2des');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('ul.ajuda3');
+    var dropDown = $('.ajuda3des');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('ul.ajuda4');
+    var dropDown = $('.ajuda4des');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('ul.ajuda5');
+    var dropDown = $('.ajuda5des');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('ul.ajuda6');
+    var dropDown = $('.ajuda6des');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
